223cd574db478a4e69febd231fa6b71cadf46db3
waspryo <wasp2123321@gmail.com>
2019-12-22 16:21:56 +0900

    get route

--- app/lib/app.js
+++ app/lib/app.js
@@ -1,5 +1,9 @@
 class App {
     constructor(selector) {
         this.appElement = document.querySelector(selector);
+        this.componentsByName = [];
+    }
+    addComponent(component) {
+        this.componentsByName[component.name] = component;
     }
 }
--- app/lib/router.js
+++ app/lib/router.js
@@ -1,5 +1,21 @@
 class Router {
     constructor(app) {
         this.app = app;
+        this.routes = [];
+        this.hashChange = this.hashChange.bind(this);
+
+        window.addEventListener('hashchange', this.hashChange)
+    }
+    addRoute(name, url) {
+        this.routes.push({
+            name,
+            url
+        })
+    }
+    hashChange() {
+        const hash = window.location.hash;
+
+        const route = this.routes.filter(route => hash.match(new RegExp(route.url)))
+        console.log(route)
     }
 }
