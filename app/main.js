1dc7b08d3d9092916e2c2dd102a1931ca52c8683
waspryo <wasp2123321@gmail.com>
2019-12-22 17:24:49 +0900

    dogTemplate

--- app/main.js
+++ app/main.js
@@ -1,13 +1,31 @@
 const app = new App('#app');
 const api = new API();
 
+const dogTemplate = (dog) => `
+<section class="dog-listing">
+  <a href="#/dogs/${dog.id}">
+    <h3 class="name">${dog.name}</h3>
+    <section>
+      <figure>
+        <img src="${dog.imageUrl}" alt="${dog.name}"/>
+        <figcaption>${dog.imageCaption}</figcaption>
+      <figure>
+       <p>${dog.description}</p>
+    <section>
+`
+
 app.addComponent({
     name: 'dogs',
     model: {
         dogs: []
     },
     view(model) {
-        return `There are ${model.dogs.length} dogs`
+        const dogsHTML = model.dogs.reduce((html, dog) => html + `<li>${dogTemplate(dog)}</li>`, '')
+        return `
+            <ul class="dogs">
+                ${dogsHTML}
+            </ul>
+        `
     },
     controller(model) {
         api.getDogs()
